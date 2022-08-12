d5a6ad40d4a33e7927695d4688302d246ccbed16
Ryan Silva <101747305+Ryansv1@users.noreply.github.com>
2022-08-12 09:23:23 -0300

    First Commit

---
+++ mainbase.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/mainbase-style.css">
+    <title>Lycooper</title>
+</head>
+
+<header>
+      <?php
+      require('partials/navbar.php');
+      ?>
+</header>
+<body>
+<section id="showmore" class="section container-fluid">
+    <h2>Bem-vindo ao Lycooper!</h2>
+    <h4>sinta-se a vontade para utilizar as ferramentas do sistema.<br>
+        Clique no botão abaixo para conhecer mais afundo o projeto!</h4>
+    <button id="showmore-b">Quero conhecer mais!</button>
+</section>
+|<section id="aboutus" class="section container-fluid d-flex">
+    <div class="text" id="title-aboutus">
+      <h3>Sobre nós:</h3>
+    </div>
+    <div class="text" id="text-aboutus">
+      <p>lalalalalalalallalalaallalala</p>
+      <p>lalalalallalalalalalallalalal</p>
+    </div>
+</section>
+<section id="bootstrapc" class="section container-fluid">
+
+</section>
+        
+<footer id="main-footer" class="text-footer text-center footer">
+        <div id="footer-brand">Lycooper</div>
+    </footer>
+</body>
+</html>
